351109e4d2c2d673755c2f3f966f6b1b3b4539f7
Marco <irotaicnoc@gmail.com>
2025-06-17 16:58:13 +0200

    added configuration file and code for reading it

---
+++ args.py
@@ -0,0 +1,53 @@
+import yaml
+import copy
+import argparse
+
+import utils
+
+
+def import_args(yaml_path: str, read_from_command_line: bool = False, **kwargs) -> dict:
+    # read data from yaml config file
+    with open(yaml_path) as f:
+        data_dict = yaml.safe_load(f)
+
+    if read_from_command_line:
+        # command line arguments have priority over yaml arguments
+        data_dict = from_command_line(default_data_dict=data_dict)
+
+    # function arguments have priority over yaml AND command line arguments
+    data_dict = from_function_arguments(default_data_dict=data_dict, **kwargs)
+
+    try:
+        verbose = data_dict['verbose']
+    except AttributeError:
+        verbose = 0
+    if verbose >= 3:
+        print(f'Yaml config file: "{yaml_path}"')
+        print('Imported parameters:')
+        utils.pretty_print_dict(data_dict)
+
+    return data_dict
+
+
+def from_command_line(default_data_dict: dict) -> dict:
+    parser = argparse.ArgumentParser()
+    for key in default_data_dict:
+        value = default_data_dict[key]
+        parser.add_argument(f'--{key}', dest=key, type=type(value))
+
+    updated_data_dict = copy.deepcopy(default_data_dict)
+    args = parser.parse_args()
+    args_dict = vars(args)
+    for key in args_dict:
+        if args_dict[key] is not None:
+            updated_data_dict[key] = args_dict[key]
+
+    return updated_data_dict
+
+
+def from_function_arguments(default_data_dict: dict, **kwargs) -> dict:
+    updated_data_dict = copy.deepcopy(default_data_dict)
+    for key in kwargs:
+        updated_data_dict[key] = kwargs[key]
+
+    return updated_data_dict
--- test_google_studio.py
+++ test_google_studio.py
@@ -1,14 +1,11 @@
 from google import genai
 from google.genai import types
 
+import args
 import utils
+import google_ai_studio_tts
 import function_declarations
 import global_constants as gc
-
-
-# get the API key from a file
-api_key_file_path = 'gemini_api_key.txt'
-api_key = utils.get_api_key(file_path=api_key_file_path)
 
 
 def get_llm_response(model_name: str, input, config=None) -> tuple:
@@ -60,8 +57,12 @@ def get_llm_response(model_name: str, input, config=None) -> tuple:
 
 
 if __name__ == '__main__':
+    parameters = args.import_args(yaml_path=gc.CONFIG_FILE_PATH)
+
+    # get the API key from a file
+    api_key = utils.get_api_key(file_path=parameters['api_key_file_path'])
     client = genai.Client(api_key=api_key)
-    model_name = 'gemini-2.0-flash'
+    model_name = parameters['brain_model_name']
     # model_name = 'gemini-2.0-flash-live-001'
     tools = types.Tool(function_declarations=function_declarations.function_list)
     config = types.GenerateContentConfig(tools=[tools])
@@ -73,6 +74,8 @@ if __name__ == '__main__':
     # text_input = 'Move the robot forward with medium speed.'
     # response = get_llm_response(model_name=model_name, input=text_input, config=config)
 
+    print('=======================================================================================')
+
     # Send the audio input to the model and get the response
     # audio_file_path = gc.DATA_FOLDER_PATH + 'hello.mp3'
     # audio_file_path = gc.DATA_FOLDER_PATH + 'backward.mp3'
@@ -83,4 +86,39 @@ if __name__ == '__main__':
         'respond with text of call a function as appropriate',
         types.Part.from_bytes(data=audio_bytes, mime_type='audio/mp3')
     ]
-    response = get_llm_response(model_name=model_name, input=audio_input, config=config)
+    is_function_call, response = get_llm_response(model_name=model_name, input=audio_input, config=config)
+    if not is_function_call:
+        audio_data = google_ai_studio_tts.text_to_speech(
+            text_input=response,
+            client=client,
+            model_name=parameters['tts_model_name'],
+            voice_name=parameters['tts_voice_name'],
+            save_file=parameters['save_audio_file']
+        )
+
+        # play the audio
+        utils.play_audio(audio_data=audio_data, sample_rate=24000)
+
+    print('=======================================================================================')
+
+    audio_file_path = gc.DATA_FOLDER_PATH + 'hello.mp3'
+    with open(audio_file_path, 'rb') as f:
+        audio_bytes = f.read()
+    audio_input = [
+        'respond with text of call a function as appropriate',
+        types.Part.from_bytes(data=audio_bytes, mime_type='audio/mp3')
+    ]
+    is_function_call, response = get_llm_response(model_name=model_name, input=audio_input, config=config)
+    if not is_function_call:
+        audio_data = google_ai_studio_tts.text_to_speech(
+            text_input=response,
+            client=client,
+            model_name=parameters['tts_model_name'],
+            voice_name=parameters['tts_voice_name'],
+            save_file=parameters['save_audio_file']
+        )
+
+        # play the audio
+        utils.play_audio(audio_data=audio_data, sample_rate=24000)
+
+    print('=======================================================================================')
