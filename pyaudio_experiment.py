98eec0fd775abc6143d7562aec5004b4d75d4cb7
Marco <irotaicnoc@gmail.com>
2025-06-21 14:40:09 +0200

    test robot speaker

--- pyaudio_experiment.py
+++ pyaudio_experiment.py
@@ -1,3 +1,4 @@
+import argparse
 from pathlib import Path
 
 import utils
@@ -11,13 +12,20 @@ for file in Path(gc.DATA_FOLDER_PATH).glob('*.mp3'):
     with open(file, 'rb') as audio_file:
         file_audio_list.append(audio_file.read())
 
+# read command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-o', '--output_device_index', type=int, default=1,)
+args = parser.parse_args()
+output_device_index = args.output_device_index
+print(f'Output device index: {output_device_index}')
+
 # play all audio files
 counter = 0
 for audio_data in file_audio_list:
     print(f'Playing audio file {counter + 1}/{len(file_audio_list)}')
     utils.play_audio(
         audio_data=audio_data,
-        output_device_index=1,
+        output_device_index=output_device_index,
         sample_rate=16000
     )
     counter += 1
