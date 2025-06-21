5b21a11f9a26d35fa936062948f9330cee25d73b
Marco <irotaicnoc@gmail.com>
2025-06-21 15:12:48 +0200

    test robot speaker

--- pyaudio_experiment.py
+++ pyaudio_experiment.py
@@ -16,12 +16,14 @@ for file in Path(gc.DATA_FOLDER_PATH).glob('*.mp3'):
 parser = argparse.ArgumentParser()
 parser.add_argument('-o', '--output_device_index', type=int, default=1,)
 parser.add_argument('-s', '--sample_rate', type=int, default=24000,)
-parser.add_argument('-c', '--sample_rate', type=int, default=24000,)
+parser.add_argument('-c', '--channels', type=int, default=1,)
 args = parser.parse_args()
 output_device_index = args.output_device_index
 sample_rate = args.sample_rate
+channels = args.channels
 print(f'Output device index: {output_device_index}')
 print(f'Sample rate: {sample_rate}')
+print(f'Channels: {channels}')
 
 # play all audio files
 counter = 0
@@ -30,7 +32,8 @@ for audio_data in file_audio_list:
     utils.play_audio(
         audio_data=audio_data,
         output_device_index=output_device_index,
-        sample_rate=sample_rate
+        sample_rate=sample_rate,
+        channels=channels,
     )
     counter += 1
 
