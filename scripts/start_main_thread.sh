574393c460bcc063f25a7d8727dad5c941572bfa
Marco <irotaicnoc@gmail.com>
2025-06-23 13:09:22 +0200

    added script for starting main thread

---
+++ scripts/start_main_thread.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+# This script activates the virtual environment and runs the main_thread.py script
+
+source ~/GIT/voice_robot_interaction/.venv/bin/activate
+python ~/GIT/voice_robot_interaction/main_thread.py
