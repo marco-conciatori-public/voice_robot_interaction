4c97355d582fa79efdbfc63fff281d0d70061a9a
Marco <irotaicnoc@gmail.com>
2025-06-23 17:58:21 +0200

    modified scripts

--- scripts/start_main_thread_terminal.sh
+++ scripts/start_main_thread_terminal.sh
@@ -2,8 +2,6 @@
 
 # This script activates the virtual environment and runs the main_thread.py script
 
-source home/jetson/GIT/voice_robot_interaction/.venv/bin/activate
-python home/jetson/GIT/voice_robot_interaction/main_thread.py
 gnome-terminal -- bash -c "source home/jetson/GIT/voice_robot_interaction/.venv/bin/activate;python home/jetson/GIT/voice_robot_interaction/main_thread.py;exec bash"
 
 wait
