c07bcf0180022f9838c4ca306e628d90fb0b76bc
Marco <irotaicnoc@gmail.com>
2025-07-15 23:10:20 +0200

    added code for passing images to Google AI Studio LLMs

--- google_ai_studio/reasoning_service.py
+++ google_ai_studio/reasoning_service.py
@@ -14,7 +14,8 @@ class ReasoningService:
                  tools: types.Tool = None,
                  prompt_template: str = None,
                  remember_history: bool = False,
-                 mime_type: str = 'audio/wav',
+                 audio_mime_type: str = 'audio/wav',
+                 image_mime_type: str = 'image/jpeg',
                  ):
         """
         Initializes the ReasoningService with the Google AI Studio client, model name, and optional configuration.
@@ -23,7 +24,8 @@ class ReasoningService:
         :param tools: types.Tool: Optional tools to use for the reasoning process, default is None.
         :param prompt_template: str: The template for the prompt, which can include instructions or context.
         :param remember_history: bool: Whether to remember the history of interactions, default is False.
-        :param mime_type: str: The MIME type of the audio data, default is 'audio/wav'.
+        :param audio_mime_type: str: The MIME type of the audio data, default is 'audio/wav'.
+        :param image_mime_type: str: The MIME type of the image data, default is 'image/jpeg'.
         """
 
         self.client = client
@@ -32,17 +34,19 @@ class ReasoningService:
         self.config = types.GenerateContentConfig(tools=[self.tools])
         self.prompt_template = prompt_template
         self.remember_history = remember_history
-        self.mime_type = mime_type
+        self.audio_mime_type = audio_mime_type
+        self.image_mime_type = image_mime_type
 
         if self.remember_history:
             self.chat = client.chats.create(model=self.model_name, config=self.config)
 
-    def reasoning(self, audio_bytes) -> tuple:
+    def reasoning(self, audio_bytes: bytes = None, image_bytes: bytes = None, **kwargs) -> tuple:
         """
-        Sends a text prompt to the Google AI Studio LLM and returns the response.
+        Sends an audio message or image to the Google AI Studio LLM and returns the response.
 
         Args:
-            audio_bytes: The prompt to send to the LLM (audio).
+            audio_bytes: the audio message to send to the LLM.
+            image_bytes: the image message to send to the LLM.
 
         Returns:
             tuple: A tuple containing:
@@ -50,26 +54,41 @@ class ReasoningService:
                 - response: The response from the LLM, which can be either text or a function call with parameters, or
                  an error message if an exception occurs.
         """
+        assert audio_bytes is not None or image_bytes is not None, 'Either audio_bytes or image_bytes must be supplied'
+        assert audio_bytes is None or image_bytes is None, 'Only one of audio_bytes or image_bytes can be supplied'
         try:
-            if self.prompt_template is None:
-                audio_input = [types.Part.from_bytes(data=audio_bytes, mime_type=self.mime_type)]
-            else:
-                audio_input = [self.prompt_template, types.Part.from_bytes(data=audio_bytes, mime_type=self.mime_type)]
+            chosen_input = None
+            if audio_bytes is not None:
+                if self.prompt_template is None:
+                    chosen_input = [types.Part.from_bytes(data=audio_bytes, mime_type=self.audio_mime_type)]
+                else:
+                    chosen_input = [self.prompt_template, types.Part.from_bytes(
+                        data=audio_bytes,
+                        mime_type=self.audio_mime_type,
+                    )]
+            elif image_bytes is not None:
+                if self.prompt_template is None:
+                    chosen_input = [types.Part.from_bytes(data=image_bytes, mime_type=self.image_mime_type)]
+                else:
+                    chosen_input = [self.prompt_template, types.Part.from_bytes(
+                        data=image_bytes,
+                        mime_type=self.image_mime_type,
+                    )]
 
             if self.remember_history:
                 if self.chat is None:
                     raise ValueError("Chat history is enabled but chat object is not initialized.")
-                response = self.chat.send_message(audio_input)
+                response = self.chat.send_message(chosen_input)
             else:  # without history
                 if self.config is None:  # without config
                     response = self.client.models.generate_content(
                         model=self.model_name,
-                        contents=audio_input,
+                        contents=chosen_input,
                     )
                 else:  # with config
                     response = self.client.models.generate_content(
                         model=self.model_name,
-                        contents=audio_input,
+                        contents=chosen_input,
                         config=self.config,
                     )
 
